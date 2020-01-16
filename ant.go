fe734c6542b13da9d54ea0a565a0905d11fee69a
Lukas Sztefek <lukas.sztefek@gmail.com>
2020-01-16 21:57:33 +0100

    create types file

--- ant.go
+++ ant.go
@@ -1,29 +1,5 @@
 package ant
 
-type Color int
-
-const (
-	White = iota
-	Black
-)
-
-type Direction int
-
-const (
-	N = iota
-	E
-	S
-	W
-)
-
-type Board [5][5]Color
-
-type Ant struct {
-	x         int
-	y         int
-	direction Direction
-}
-
 func Step(actualBoard Board, actualAnt Ant) (Board, Ant) {
 	return Board{}, Ant{}
 }
--- ant_test.go
+++ ant_test.go
@@ -11,13 +11,30 @@ func TestStep(t *testing.T) {
 	}
 
 	testCases := []testCase{
-		testCase{},
+		testCase{
+			actualBoard: Board{
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+			},
+			actualAnt: Ant{2, 2, N},
+			nextBoard: Board{
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+				{White, White, Black, White, White},
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+			},
+			nextAnt: Ant{3, 2, N},
+		},
 	}
 
 	for _, tc := range testCases {
 		gotBoard, gotAnt := Step(tc.actualBoard, tc.actualAnt)
 		if gotBoard != tc.nextBoard || gotAnt != tc.nextAnt {
-			t.Errorf("Step(%v, %v) = %v, %v want %v, %v",
+			t.Errorf("Step(\n%v%v\n) =\n%v%v \n\nwant\n\n%v%v",
 				tc.actualBoard, tc.actualAnt, gotBoard, gotAnt, tc.nextBoard, tc.nextAnt)
 		}
 	}
