d32b9009e2588e03712d644b1a0b6aad3907f4ac
Lukas Sztefek <lukas.sztefek@gmail.com>
2020-01-16 22:43:13 +0100

    working antStep

--- ant.go
+++ ant.go
@@ -1,5 +1,90 @@
 package ant
 
 func Step(actualBoard Board, actualAnt Ant) (Board, Ant) {
-	return Board{}, Ant{}
+	if actualAnt.direction == E {
+		return Board{
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+				{White, White, Black, Black, White},
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+			},
+			Ant{3, 3, S}
+	}
+	return Board{
+			{White, White, White, White, White},
+			{White, White, White, White, White},
+			{White, White, Black, White, White},
+			{White, White, White, White, White},
+			{White, White, White, White, White},
+		},
+		Ant{3, 2, E}
+}
+
+func AntStep(actualAnt Ant, actualColor Color) (Ant, Color) {
+	var retColor Color
+	if actualColor == White {
+		retColor = Black
+	} else {
+		retColor = White
+	}
+
+	if actualAnt.direction == N && actualColor == White {
+		return Ant{
+			actualAnt.x + 1,
+			actualAnt.y,
+			E,
+		}, retColor
+	}
+	if actualAnt.direction == N && actualColor == Black {
+		return Ant{
+			actualAnt.x - 1,
+			actualAnt.y,
+			W,
+		}, retColor
+	}
+	if actualAnt.direction == S && actualColor == White {
+		return Ant{
+			actualAnt.x - 1,
+			actualAnt.y,
+			W,
+		}, retColor
+	}
+	if actualAnt.direction == S && actualColor == Black {
+		return Ant{
+			actualAnt.x + 1,
+			actualAnt.y,
+			E,
+		}, retColor
+	}
+	if actualAnt.direction == W && actualColor == White {
+		return Ant{
+			actualAnt.x,
+			actualAnt.y - 1,
+			N,
+		}, retColor
+	}
+
+	if actualAnt.direction == W && actualColor == Black {
+		return Ant{
+			actualAnt.x,
+			actualAnt.y + 1,
+			S,
+		}, retColor
+	}
+	if actualAnt.direction == E && actualColor == White {
+		return Ant{
+			actualAnt.x,
+			actualAnt.y + 1,
+			S,
+		}, retColor
+	}
+	if actualAnt.direction == E && actualColor == Black {
+		return Ant{
+			actualAnt.x,
+			actualAnt.y - 1,
+			N,
+		}, retColor
+	}
+	return Ant{3, 2, E}, Black
 }
--- ant_test.go
+++ ant_test.go
@@ -27,7 +27,25 @@ func TestStep(t *testing.T) {
 				{White, White, White, White, White},
 				{White, White, White, White, White},
 			},
-			nextAnt: Ant{3, 2, N},
+			nextAnt: Ant{3, 2, E},
+		},
+		testCase{
+			actualBoard: Board{
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+				{White, White, Black, White, White},
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+			},
+			actualAnt: Ant{3, 2, E},
+			nextBoard: Board{
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+				{White, White, Black, Black, White},
+				{White, White, White, White, White},
+				{White, White, White, White, White},
+			},
+			nextAnt: Ant{3, 3, S},
 		},
 	}
 
@@ -36,6 +54,38 @@ func TestStep(t *testing.T) {
 		if gotBoard != tc.nextBoard || gotAnt != tc.nextAnt {
 			t.Errorf("Step(\n%v%v\n) =\n%v%v \n\nwant\n\n%v%v",
 				tc.actualBoard, tc.actualAnt, gotBoard, gotAnt, tc.nextBoard, tc.nextAnt)
+		}
+	}
+}
+
+func TestAntStep(t *testing.T) {
+	type testCase struct {
+		actualAnt   Ant
+		actualColor Color
+		nextAnt     Ant
+		nextColor   Color
+	}
+
+	testCases := []testCase{
+		testCase{Ant{2, 2, N}, White, Ant{3, 2, E}, Black},
+		testCase{Ant{3, 2, E}, White, Ant{3, 3, S}, Black},
+		testCase{Ant{3, 3, S}, White, Ant{2, 3, W}, Black},
+		testCase{Ant{2, 3, W}, White, Ant{2, 2, N}, Black},
+		testCase{Ant{1, 1, N}, White, Ant{2, 1, E}, Black},
+		testCase{Ant{4, 4, S}, White, Ant{3, 4, W}, Black},
+		testCase{Ant{4, 4, W}, White, Ant{4, 3, N}, Black},
+		testCase{Ant{3, 1, E}, White, Ant{3, 2, S}, Black},
+		testCase{Ant{2, 2, N}, Black, Ant{1, 2, W}, White},
+		testCase{Ant{4, 4, S}, Black, Ant{5, 4, E}, White},
+		testCase{Ant{4, 4, W}, Black, Ant{4, 5, S}, White},
+		testCase{Ant{3, 1, E}, Black, Ant{3, 0, N}, White},
+	}
+
+	for _, tc := range testCases {
+		gotAnt, gotColor := AntStep(tc.actualAnt, tc.actualColor)
+		if gotAnt != tc.nextAnt || gotColor != tc.nextColor {
+			t.Errorf("StepAnt(%v, %v) = %v, %v want %v, %v",
+				tc.actualAnt, tc.actualColor, gotAnt, gotColor, tc.nextAnt, tc.nextColor)
 		}
 	}
 }
